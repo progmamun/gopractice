fdd5edcf10776f1b50ba47ab896520a22af465bd
progmamun <mamun333089@gmail.com>
2021-10-13 23:10:29 +0600

    TCP_connections

--- Network Programming/tcp_connections/main.go
+++ Network Programming/tcp_connections/main.go
@@ -1,85 +1,59 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"net/http"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"time"
 )
-conn, err := net.Dial("tcp", "localhost:8080")
-
-addr, err := net.ResolveTCPAddr("tcp", "localhost:8080")
-if err != nil {
-// handle error
-}
-conn, err := net.DialTCP("tcp", nil, addr)
-if err != nil {
-// handle error
-}
-
-func main() {
-	if len(os.Args) != 2 {
-	log.Fatalln("Please specify an address.")
-	}
-	addr, err := net.ResolveTCPAddr("tcp", os.Args[1])
-	if err != nil {
-	log.Fatalln("Invalid address:", os.Args[1], err)
-	}
-	listener, err := net.ListenTCP("tcp", addr)
-	if err != nil {
-	log.Fatalln("Listener:", os.Args[1], err)
-	}
-	log.Println("<- Listening on", addr)
-	go createConn(addr)
-	conn, err := listener.AcceptTCP()
-	if err != nil {
-	log.Fatalln("<- Accept:", os.Args[1], err)
-	}
-	handleConn(conn)
-}
 
 func createConn(addr *net.TCPAddr) {
-		defer log.Println("-> Closing")
-		conn, err := net.DialTCP("tcp", nil, addr)
-		if err != nil {
+	defer log.Println("-> Closing")
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
 		log.Fatalln("-> Connection:", err)
-		}
-		log.Println("-> Connection to", addr)
-		r := bufio.NewReader(os.Stdin)
-		for {
+	}
+	log.Println("-> Connection to", addr)
+	r := bufio.NewReader(os.Stdin)
+	for {
 		fmt.Print("# ")
 		msg, err := r.ReadBytes('\n')
 		if err != nil {
-		log.Println("-> Message error:", err)
+			log.Println("-> Message error:", err)
 		}
 		if _, err := conn.Write(msg); err != nil {
-		log.Println("-> Connection:", err)
-		return
+			log.Println("-> Connection:", err)
+			return
 		}
-		}
+	}
 }
 
 func handleConn(conn net.Conn) {
-			r := bufio.NewReader(conn)
-			time.Sleep(time.Second / 2)
-			for {
-			msg, err := r.ReadString('\n')
-			if err != nil {
+	r := bufio.NewReader(conn)
+	time.Sleep(time.Second / 2)
+	for {
+		msg, err := r.ReadString('\n')
+		if err != nil {
 			log.Println("<- Message error:", err)
 			continue
-			}
-			switch msg = strings.TrimSpace(msg); msg {
-			case `\q`:
+		}
+		switch msg = strings.TrimSpace(msg); msg {
+		case `\q`:
 			log.Println("Exiting...")
 			if err := conn.Close(); err != nil {
-			log.Println("<- Close:", err)
+				log.Println("<- Close:", err)
 			}
 			time.Sleep(time.Second / 2)
 			return
-			case `\x`:
+		case `\x`:
 			log.Println("<- Special message `\\x` received!")
-			default:
+		default:
 			log.Println("<- Message Received:", msg)
-			}
-			}
+		}
+	}
 }
 
 /*func main() {
@@ -113,5 +87,5 @@ func main() {
 					log.Fatalln("Invalid address:", os.Args[1], err)
 					}
 					createConn(addr)
-}				
+}
 */
