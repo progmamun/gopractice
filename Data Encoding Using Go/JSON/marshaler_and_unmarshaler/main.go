7d3d8842417a36b1c2b64768da8c38fa5eb4da92
progmamun <mamun333089@gmail.com>
2021-10-14 15:16:49 +0600

    UnmarshalJSON

---
+++ Data Encoding Using Go/JSON/marshaler_and_unmarshaler/main.go
@@ -0,0 +1,45 @@
+package main
+
+/* type Marshaler interface {
+MarshalJSON() ([]byte, error)
+}
+type Unmarshaler interface {
+UnmarshalJSON([]byte) error
+} */
+
+import (
+	"encoding/json"
+	"log"
+	"strings"
+)
+
+type Character struct {
+	Name        string `json:"name"`
+	Surname     string `json:"surname"`
+	Job         string `json:"job,omitempty"`
+	YearOfBirth int    `json:"year_of_birth,omitempty"`
+}
+
+func (c *Character) UnmarshalJSON(b []byte) error {
+	log.Println("Calling UnmarshalJSON")
+	type C Character
+	var v C
+	if err := json.Unmarshal(b, &v); err != nil {
+		return err
+	}
+	*c = Character(v)
+	if c.Job == "" {
+		c.Job = "unknown"
+	}
+	return nil
+}
+
+func main() {
+	r := strings.NewReader(`{"name":"Lavinia","surname":"Whateley","year_of_birth":1878}`)
+	d := json.NewDecoder(r)
+	var c Character
+	if err := d.Decode(&c); err != nil {
+		log.Fatalln(err)
+	}
+	log.Printf("%+v", c)
+}
