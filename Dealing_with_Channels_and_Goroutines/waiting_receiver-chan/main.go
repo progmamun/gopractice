2aa6fb1f9b55fdb17a86659738a34a95e24dc57d
progmamun <mamun333089@gmail.com>
2021-10-14 18:24:39 +0600

    waiting_receiver-channels

---
+++ Dealing_with_Channels_and_Goroutines/waiting_receiver-chan/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func send(ch chan<- int, max int) {
+	for i := 0; i < max; i++ {
+		ch <- i
+	}
+	close(ch)
+}
+
+func receive(ch <-chan int, done chan<- struct{}) {
+	for v := range ch {
+		fmt.Println(v)
+	}
+	close(done)
+}
+
+func main() {
+	a := make(chan int)
+	go send(a, 10)
+	done := make(chan struct{})
+	go receive(a, done)
+	<-done
+}
