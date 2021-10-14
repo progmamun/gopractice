ee245fcb00f1f4ef3a1591e57788fa47250cb9ff
progmamun <mamun333089@gmail.com>
2021-10-14 17:36:47 +0600

    synchronization

---
+++ Dealing_with_Channels_and_Goroutines/synchronization/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var i int
+	go func(i *int) {
+		for j := 0; j < 20; j++ {
+			time.Sleep(time.Millisecond)
+			fmt.Println(*i, j)
+		}
+	}(&i)
+	for i = 0; i < 20; i++ {
+		time.Sleep(time.Millisecond)
+		fmt.Println(i)
+	}
+}
