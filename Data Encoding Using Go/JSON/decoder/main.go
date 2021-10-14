dce2283e16bbbc8165b8f8f05284d9c7576ed486
progmamun <mamun333089@gmail.com>
2021-10-14 13:52:14 +0600

    json_decoder

---
+++ Data Encoding Using Go/JSON/decoder/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"strings"
+)
+
+type Character struct {
+	Name        string `json:"name"`
+	Surname     string `json:"surname"`
+	Job         string `json:"job,omitempty"`
+	YearOfBirth int    `json:"year_of_birth,omitempty"`
+}
+
+func main() {
+	r := strings.NewReader(`{"name":"Lavinia","surname":"Whateley","year_of_birth":1878}`)
+	d := json.NewDecoder(r)
+	var c Character
+	if err := d.Decode(&c); err != nil {
+		log.Fatalln(err)
+	}
+	log.Printf("%+v", c)
+}
