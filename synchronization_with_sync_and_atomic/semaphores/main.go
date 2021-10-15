a50d6cf00b1265e29dfbff19f23d7ef81229c326
progmamun <mamun333089@gmail.com>
2021-10-15 16:16:56 +0600

    semaphores

---
+++ synchronization_with_sync_and_atomic/semaphores/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+)
+
+func main() {
+	s := semaphore.NewWeighted(int64(10))
+	ctx := context.Background()
+	for i := 0; i < 20; i++ {
+		if err := s.Acquire(ctx, 1); err != nil {
+			log.Fatal(err)
+		}
+		go func(i int) {
+			fmt.Println(i)
+			s.Release(1)
+		}(i)
+	}
+	time.Sleep(time.Second)
+}
