8189d2a76e2b21f4f79f6243bec47674141d538a
progmamun <mamun333089@gmail.com>
2021-10-18 09:03:05 +0600

    maps -

--- Using_Reflection/maps/main.go
+++ Using_Reflection/maps/main.go
@@ -18,3 +18,29 @@ func main() {
 		fmt.Printf("%s - k:%-10s v:%-10s\n", m, t.Key(), t.Elem())
 	}
 }
+
+/*
+func main() {
+	m := map[string]int64{}
+	v := reflect.ValueOf(m)
+
+	// setting one field
+	v.SetMapIndex(reflect.ValueOf("key"), reflect.ValueOf(int64(1000)))
+
+	fmt.Println(m)
+}
+
+
+func main() {
+	m := map[string]int64{"a": 10}
+	fmt.Println(m, len(m))
+
+	v := reflect.ValueOf(m)
+
+	// deleting field
+	v.SetMapIndex(reflect.ValueOf("a"), reflect.Value{})
+
+	fmt.Println(m, len(m))
+}
+
+*/
